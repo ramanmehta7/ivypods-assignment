3caeb11ea9a51372b92b80c68471a39363094dbb
Raman Mehta <ramanmehta833@gmail.com>
2019-11-23 23:54:41 +0530

    finish

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+const mutter = require("mutter");
+const axios = require("axios");
+const FormData = require("form-data");
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
+const path = require("path");
+
+const CLOUDINARY_API_URL =
+  "https://api.cloudinary.com/v1_1/dxjw59s8q/image/upload";
+const CLOUDINARY_PRESET = "ub8x4sqg";
+
+cloudinary.config({
+  cloud_name: "dxjw59s8q",
+  api_key: "368959491475955",
+  api_secret: "mSsNmFWiWXb2-0hmq6PtgFoE4xM"
+});
+
+const app = express();
+
+app.listen(8000, () => {
+  console.log("server running");
+});
+
+app.get("/", (req, res, next) => {
+  const url = req.query.val;
+  console.log("url : " + url);
+
+  let storage = path.resolve(__dirname, "files", "images.jpg");
+  let x = storage.toString();
+
+
+  axios({
+    url: url,
+    method: "GET",
+    responseType: "stream"
+  }).then(resp => {
+    resp.data.pipe(fs.createWriteStream(storage));
+    cloudinary.uploader.upload(x, function(err, result) {
+      console.log(err);
+      console.log('Image Uploaded');
+    });
+  });
+
+  return res.json({
+    message: "Image Uploaded",
+  });
+});
